b310867b472394f2feb56108d8d056506cfa0fe0
hanqinilnix <77395961+hanqinilnix@users.noreply.github.com>
2023-07-15 13:14:21 +0000

    Deploying to gh-pages from @ hanqinilnix/ilnix-extensions@6923c3484d412f6a7b87fd2e3f6de3b70c7dca37 🚀

--- master/Baozimh/index.js
+++ master/Baozimh/index.js
@@ -542,7 +542,7 @@ class Baozimh {
             throw Error(errorMessage);
         }
         else if (sections == 2) {
-            chapters = $(".l-box > .pure-g > div > a").toArray(); // for manhua with less than certain number of chapters
+            chapters = $(".comics-chapters__item").toArray(); // for manhua with less than certain number of chapters
         }
         else {
             chapters = $(".l-box > .pure-g[id^=chapter] > div > a").toArray();
