2110cdcd1bd9a27a186702c22c59f8f791da2c50
hanqinilnix <onghanqin@ymail.com>
2023-07-23 18:28:36 +0800

    Test Happymh getChapters

--- src/Happymh/Happymh.ts
+++ src/Happymh/Happymh.ts
@@ -93,8 +93,8 @@ export class Happymh implements ChapterProviding, HomePageSectionsProviding, Man
         this.CloudFlareError(response.status);
         const $ = this.cheerio.load(response.data as string);
 
-        const mangaDetails = $('mip-data#data > script').eq(2).text().trim();
-        throw new Error(mangaDetails);
+        const mangaDetails = $('mip-data#data').eq(2).text().trim();
+        throw new Error(`MangaDetails: ${mangaDetails}`);
         type ChapterType = {
             id: string,
             chapterName: string,
