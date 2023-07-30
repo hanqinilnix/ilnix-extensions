6e3b42654600f350bf5530c8bfae7e9495b88b40
hanqinilnix <77395961+hanqinilnix@users.noreply.github.com>
2023-07-30 06:08:10 +0000

    Deploying to gh-pages from @ hanqinilnix/ilnix-extensions@f03daf07ff59a1a504d24e5750ceb1f9cd2195f4 🚀

--- master/AsuraScans/source.js
+++ master/AsuraScans/source.js
@@ -1532,7 +1532,7 @@ class AsuraScans {
             image: $(manga).find('img').attr('src').trim()
         }));
         sectionCallback(featuredSection);
-        throw new Error($('.slide-item').eq(1).find('img').attr('src'));
+        throw new Error($('.slide-item').eq(1).find('img').attr('data-lazy-src'));
         // Popular Today
         const popularTodaySection = App.createHomeSection({
             id: 'popular_today',
